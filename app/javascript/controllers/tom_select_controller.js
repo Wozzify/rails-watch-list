502240f2e6026ef55253589709633a6879371b31
Ted Lindsay <tedlindsay@outlook.com>
2024-09-27 13:39:54 +0100

    done

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,6 +3,11 @@ import TomSelect from "tom-select"
 
 export default class extends Controller {
   connect() {
-    new TomSelect(this.element, {})
+    new TomSelect(this.element, {
+      maxOptions: null, // Set to null to display all options
+      maxItems: null,   // Set to null to allow selecting all options
+      plugins: ['remove_button'], // Optional: Add plugins as needed
+      // Additional configuration options if needed
+    })
   }
 }
